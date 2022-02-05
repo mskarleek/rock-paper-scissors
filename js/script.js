2952e4e5c89b1b088c9d731ce25b34118d4ae6cb
Karlee Kategianes <ms.karleek@gmail.com>
2022-02-05 16:48:43 -0600

    Created function to display results once round has started and made other tweaks to accomadate using images instead of buttons

--- js/script.js
+++ js/script.js
@@ -37,43 +37,57 @@ function gameRound(playerDecision) {
     if (playerDecision === computerDecision) {
         Result.innerHTML = "It's a tie!";
         checkWinner(playerScore, computerScore);
+        resultsDisplay();
     } else if (playerDecision === "Rock" && computerDecision === "Paper") {
             Result.innerHTML = "You lose! Paper beats Rock."
             computerScore += 1
             computerTally.innerHTML = "<b>Computer:</b> " + computerScore;
             moves++;
             checkWinner(playerScore, computerScore);
+            resultsDisplay();
     } else if (playerDecision === "Rock" && computerDecision === "Scissors") {
             Result.innerHTML = "You win! Rock beats Scissors."
             playerScore += 1
             playerTally.innerHTML = "<b>Player:</b> " + playerScore;
             moves++;
             checkWinner(playerScore, computerScore);
+            resultsDisplay();
     } else if (playerDecision === "Paper" && computerDecision === "Rock") {
             Result.innerHTML = "You win! Paper beats Rock."
             playerScore += 1
             playerTally.innerHTML = "<b>Player:</b> " + playerScore;
             moves++;
             checkWinner(playerScore, computerScore);
+            resultsDisplay();
     } else if (playerDecision === "Paper" && computerDecision === "Scissors") {
             Result.innerHTML = "You lose! Scissors beats Paper."
             computerScore += 1;
             computerTally.innerHTML = "<b>Computer:</b> " + computerScore;
             moves++;
             checkWinner(playerScore, computerScore);
+            resultsDisplay();
     } else if (playerDecision === "Scissors" && computerDecision === "Rock") {
             Result.innerHTML = "You lose! Rock beats Scissors."
             computerScore += 1
             computerTally.innerHTML = "<b>Computer:</b> " + computerScore;
             moves++;
             checkWinner(playerScore, computerScore);
+            resultsDisplay();
     } else if (playerDecision === "Scissors" && computerDecision === "Paper") {
             Result.innerHTML = "You win! Scissors beats Paper."
             playerScore += 1
             playerTally.innerHTML = "<b>Player:</b> " + playerScore;
             moves++;
             checkWinner(playerScore, computerScore);
+            resultsDisplay();
     }
+
+// Display results //
+
+function resultsDisplay() {
+    var results = document.getElementById("results");
+    results.style.display = "block";
+  }
 
 // Determine if a winner has been decided //
 
@@ -81,17 +95,17 @@ function checkWinner() {
     if (playerScore === 5 || computerScore === 5) {
         if (playerScore > computerScore) {
             gameOver.innerHTML = "You win the game! Refresh to play again."
-            document.querySelectorAll("button").forEach((item) => {
+            document.querySelectorAll("input").forEach((item) => {
                 item.disabled = true;
               });
         } else if (playerScore < computerScore) {
             gameOver.innerHTML = "You lose the game! Refresh to play again."
-            document.querySelectorAll("button").forEach((item) => {
+            document.querySelectorAll("input").forEach((item) => {
                 item.disabled = true;
               });
         } else if (playerScore === computerScore) {
             gameOver.innerHTML = "It's a tie! Refresh to play again."
-            document.querySelectorAll("button").forEach((item) => {
+            document.querySelectorAll("input").forEach((item) => {
                 item.disabled = true;
               });
         }
@@ -101,7 +115,7 @@ function checkWinner() {
 // Establish the player and computer choices //
 // and play a round //
 
-document.querySelectorAll("button").forEach((item) => {
+document.querySelectorAll("input").forEach((item) => {
     item.addEventListener("click", (e) => {
         playerDecision = e.target.id;
         computerDecision = computerSelection();
